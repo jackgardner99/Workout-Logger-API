bff75afe92c47869c38629fb352511f1c6f68c11
Jack Gardner <jtgardner99@gmail.com>
2026-05-08 16:02:02 -0500

    tested with yaak

--- seed_database.sh
+++ seed_database.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python manage.py migrate
-python manage.py loaddata users tokens intensity muscle_groups categories exercises
+python manage.py loaddata users tokens intensity muscle_groups categories exercises muscle_exercises
