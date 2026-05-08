d01f5c687162618d3a6d98aae3d53946cecb5298
Jack Gardner <jtgardner99@gmail.com>
2026-05-08 15:19:47 -0500

    exercise and log_exercise models made

--- workout_logger_api/models/__init__.py
+++ workout_logger_api/models/__init__.py
@@ -1,2 +1,4 @@
+from workout_logger_api.models.exercise import Category, Exercise, MuscleExercise, MuscleGroup
 from workout_logger_api.models.intensity import Intensity
+from workout_logger_api.models.log_exercise import LogExercise
 from workout_logger_api.models.workout_log import WorkoutLog
---
+++ workout_logger_api/models/exercise.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class MuscleGroup(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Exercise(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name="exercises")
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    difficulty = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MuscleExercise(models.Model):
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE, related_name="muscle_exercises")
+    muscle_group = models.ForeignKey(MuscleGroup, on_delete=models.CASCADE, related_name="muscle_exercises")
+
+    class Meta:
+        unique_together = ("exercise", "muscle_group")
