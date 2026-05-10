2e3942320a4ede5142529880cc31ef2ec3ed4ee9
Jack Gardner <jtgardner99@gmail.com>
2026-05-10 10:50:58 -0500

    commumity feed fetch success

--- seed_database.sh
+++ seed_database.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python manage.py migrate
-python manage.py loaddata users tokens intensity muscle_groups categories exercises muscle_exercises
+python manage.py loaddata users tokens intensity muscle_groups categories exercises muscle_exercises workout_logs log_exercises
